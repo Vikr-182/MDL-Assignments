0718d5b93f1796b521c40f79989af8aa04a84588
qureshi.nomaan <nqafzal@gmail.com>
2020-03-22 00:00:55 +0530

    some progress

--- 3Assignment/set_cur_state.py
+++ 3Assignment/set_cur_state.py
@@ -1,9 +1,9 @@
 #======================================================================================#
-# Function to set the current state #
+# Function to set and get the current state #
 #======================================================================================#
 import pickle
 import numpy as np
-# state = [list(np.zeros(11)) for x in range(5)]
+state = [list(np.zeros(11)) for x in range(5)]
 dim_state = 5
 
 def set_current_state(state):
@@ -17,6 +17,23 @@ def set_current_state(state):
                 return
     with open('current_state.pkl', 'wb') as f:
         pickle.dump(state, f)
+def get_current_state() :
+    with open('current_state.pkl', 'rb') as f:
+        array = pickle.load(f)
+    return array
+    
+def set_initial_state() :
+    overfit_state = [-0.00016927573251173823, 0.0010953590656607808, 0.003731869524518327, 0.08922889556431182, 0.03587507175384199, -0.0015634754169704097,
+                        -7.439827367266828e-05, 3.7168210026033343e-06, 1.555252501348866e-08, -2.2215895929103804e-09, 2.306783174308054e-11]
+    initial_state = np.random.uniform(low = -1, high = 1, size =(5, 11))
 
-# set_current_state(state)
+    j = 0
+    for i in range(5):
+        initial_state[i][j:j+2] = overfit_state[j:j+2]
+        j+=2
+    print(initial_state)
+    set_current_state(initial_state)
+    return
+
+# set_initial_state()
 #======================================================================================#
--- 3Assignment/solution.py
+++ 3Assignment/solution.py
@@ -1,8 +1,45 @@
 import numpy as np
 from client import get_errors, submit
+from set_cur_state import set_current_state, get_current_state
+import random
 
 overfit_state = [-0.00016927573251173823, 0.0010953590656607808, 0.003731869524518327, 0.08922889556431182, 0.03587507175384199, -0.0015634754169704097,
                     -7.439827367266828e-05, 3.7168210026033343e-06, 1.555252501348866e-08, -2.2215895929103804e-09, 2.306783174308054e-11]
-init_state = np.random.uniform(low = -1, high=1, size=(11,))
-err = get_errors('4wbRlSOEbHj0HmuzgNcnNc6KeW9ERzSsccXuswFyGlkn7bUMol', overfit_state)
-print(err)
+
+
+
+iter = 1000
+for i in range(iter) :
+    cur_state = get_current_state()
+    new_state = [[0 for x in range(11)] for y in range(5)]
+    count = 0
+    errs = []
+    for state in cur_state :
+        err = get_errors('4wbRlSOEbHj0HmuzgNcnNc6KeW9ERzSsccXuswFyGlkn7bUMol',
+                            list(state))
+        errs.append([err, count])
+        count += 1
+    errs.sort()
+    # print(errs)
+    # Crossover 0-1, 0-2, 0-3, 1-2, 1-3
+    print(errs)
+    num_state = 0
+    for j in range(2) :
+        for k in range(j+1, 4):
+            for x in range(11) :
+                cross_prob = random.uniform(0, 1)
+                mutation_prob = random.uniform(0, 1)
+                mutated = random.uniform(-0.00000000001, 0.00000000001)
+                if cross_prob < 0.8 :
+                    new_state[num_state][x] = cur_state[errs[j][1]][x]
+                else :
+                    new_state[num_state][x] = cur_state[errs[k][1]][x]
+                
+                if mutation_prob < 0.3 :
+                    new_state[num_state][x] = mutated
+            num_state += 1
+    # print("Old State", cur_state)
+    # print("New state : ", new_state)
+    set_current_state(new_state)
+    
+# print(err)
