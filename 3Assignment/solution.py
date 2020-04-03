3f112cffcb495d3251556226353d222dd85dec37
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2020-04-04 00:01:49 +0530

    okna

--- 3Assignment/solution.py
+++ 3Assignment/solution.py
@@ -9,19 +9,23 @@ overfit_state = [-0.00016927573251173823, 0.0010953590656607808, 0.0037318695245
 
 
 
-iter = 20
+iter = 40
 mut_state = [[0 for x in range(11)] for y in range(5)]
 temp_err = [[[1000000000000000000, 1000000000000000000], 0] for x in range(5)]
 temp_state = None
 for i in range(iter) :
     cur_state = get_current_state()
     # print("Old State ", cur_state)
-    # print(cur_state)
+    print("Generation : ", i+1)
+    print("\nCurrent Vectors : ")
+    for x in cur_state :
+        print("Vector  :", x)
+    
     new_state = [[0 for x in range(11)] for y in range(5)]
     count = 0
     errs = []
     
-    
+
     for state in cur_state :
         err = get_errors('4wbRlSOEbHj0HmuzgNcnNc6KeW9ERzSsccXuswFyGlkn7bUMol',
                             list(state))
@@ -36,7 +40,7 @@ for i in range(iter) :
     # if temp_err != None:
     #     for x in range(0, 2) :
     #         if errs[x][0][0] > temp_err[x+1][0][0] :
-    #             print("Err : ", temp_err[x+1][0][0])
+    #             # print("Err : ", temp_err[x+1][0][0])
     #             cur_state[x] = temp_state[temp_err[x+1][1]]
     #             errs[x][0] = temp_err[x+1][0]
     #
@@ -59,10 +63,11 @@ for i in range(iter) :
     
     # print(errs)
     # Crossover 0-1, 0-2, 0, 1-2, 0 with mutations
-    print()
-    print("Iter : ", i)
+    # print()
+    # print("Iter : ", i)
     # print(train_err)
-    print(errs)
+    print("\n\nErrors : \n", errs)
+    print()
     num_state = 0
     for j in range(2) :
         for k in range(j+1, 4):
@@ -84,11 +89,11 @@ for i in range(iter) :
                 current = new_state[num_state][x]
                 
                 if current > 0 :
-                    # mutated = random.uniform(max(-1, current*0.01), min(1, current*1.8))
-                    mutated = random.uniform(-10**-state_range[x], 10**-state_range[x])
+                    mutated = random.uniform(max(-1, current*0.95), min(1, current*1.05))
+                    # mutated = random.uniform(-10**-state_range[x], 10**-state_range[x])
                 else :
-                    # mutated = random.uniform(max(-1, current*1.8), min(1, current*0.01))
-                    mutated = random.uniform(-10**-state_range[x], 10**-state_range[x])
+                    mutated = random.uniform(max(-1, current*1.05), min(1, current*0.95))
+                    # mutated = random.uniform(-10**-state_range[x], 10**-state_range[x])
                 if mutation_prob < mutation_keep_prob :
                     new_state[num_state][x] = mutated
                     
@@ -99,10 +104,10 @@ for i in range(iter) :
         mutated_keep_prob = 0.1
         current = cur_state[errs[0][1]][x]
         if current > 0 :
-            mutated = random.uniform(max(-1, current*0.01), min(1, current*1.8))
+            mutated = random.uniform(max(-1, current*0.95), min(1, current*1.05))
             # mutated = random.uniform(-10**-state_range[x], 10**-state_range[x])
         else :
-            mutated = random.uniform(max(-1, current*1.8), min(1, current*0.01))
+            mutated = random.uniform(max(-1, current*1.05), min(1, current*0.95))
             # mutated = random.uniform(-10**-state_range[x], 10**-state_range[x])
         if mutated <= -10 or mutated >= 10 :
             mutated = random.uniform(-10**-state_range[x], 10**-state_range[x])
@@ -113,14 +118,14 @@ for i in range(iter) :
         else :
             new_state[4][x] = cur_state[errs[0][1]][x]
     
-    if iter %5 == 0 :
+    if iter %10 == 0 :
         min_err = 1000000000000000000
         pos = 0
         for x in range(4) :
             if errs[x][0][1] < min_err :
                 min_err = errs[x][0][1]
                 pos = x
-        print(new_state[errs[pos][1]])
+        # print(new_state[errs[pos][1]])
         submit('4wbRlSOEbHj0HmuzgNcnNc6KeW9ERzSsccXuswFyGlkn7bUMol', new_state[2])
     
         
